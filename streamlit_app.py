2047aed4e8d5122eb9a5d51de08e09ddddb7c540
VarshaMudgal <125606206+VarshaMudgal@users.noreply.github.com>
2023-05-02 09:20:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,16 +52,25 @@ except URLError as e:
 
 #Output it to the screen as a table
 
+#Move the Fruit Load List Query and Load into a Button Action
+streamlit.header("The Fruit List Contains:")
+#Snowflake related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur
+  my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+  return my_cur.fetchall()
+
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+#my_cur = my_cnx.cursor()
+#my_data_rows = my_cur.fetchall()
 
 #don't execute anything past this line while I am working to fix
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit List Contains:")
-streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add some fruits to the list
 streamlit.header("This is the challenge Fruit List:")

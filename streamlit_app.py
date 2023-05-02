4f7e107395b69843c37593b4c0077f93e5747559
VarshaMudgal <125606206+VarshaMudgal@users.noreply.github.com>
2023-05-02 08:34:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,5 @@ streamlit.header("This is the challenge Fruit List:")
 add_my_fruit = streamlit.text_input('What fruit challenge would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding',add_my_fruit)
 
-#streamlit.write('The user entered',add_my_fruit)
+#Snowflake Control of the flow addition 
+my_cur.execute("insert into fruit_load_list values('from Streamlit')")
